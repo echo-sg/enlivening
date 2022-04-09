3c4d90af3111a07251f31b5d39c6873df93a3503
Shrey Gupta <shreygupta@pop-os.localdomain>
2022-04-09 21:30:12 +0530

    Twitter package installed, readme cleared

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
 const Popup = () => {
@@ -8,16 +7,13 @@ const Popup = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/options.html" 
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          Click here to checkout the Options Page!
         </a>
       </header>
     </div>
