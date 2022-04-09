502fe8a55d7266e97d37642527b14fbbe862d38e
sumitshinde2608 <shindesumit268@gmail.com>
2022-04-10 01:56:34 +0530

    Table done

--- src/pages/Options/Options.jsx
+++ src/pages/Options/Options.jsx
@@ -49,29 +49,54 @@ const columns = [
   },
 ];
 const rows = [
-  { id: 1, userName: 'Jon', avatar: '' },
-  { id: 2, userName: 'Cersei', avatar: '' },
-  { id: 3, userName: 'Jaime', avatar: '' },
-  { id: 4, userName: 'Arya', avatar: '' },
-  { id: 5, userName: 'Daenerys', avatar: '' },
+  { id: 1, userName: '_shreygupta', name: 'Shrey Gupta' },
+  { id: 2, userName: 'nafees87n', name: 'Nafees Nehar ' },
+  { id: 3, userName: 'sumitshinde2608', name: 'Sumit Shinde' },
+  { id: 4, userName: '4molydenum2', name: 'Tathagat Paul' },
+  { id: 5, userName: 'maybe_anurag', name: 'Anurag' },
+  { id: 6, userName: '_shreygupta', name: 'Shrey Gupta' },
+  { id: 7, userName: 'nafees87n', name: 'Nafees Nehar ' },
+  { id: 8, userName: 'sumitshinde2608', name: 'Sumit Shinde' },
+  { id: 9, userName: '4molydenum2', name: 'Tathagat Paul' },
+  { id: 10, userName: 'maybe_anurag', name: 'Anurag' },
 ];
 
-// interface Props {
-//   title: string;
-// }
-
 const Options = () => {
-  // return <div className="OptionsContainer">Hello World</div>;
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        // checkboxSelection
-        // disableSelectionOnClick
-      />
+    <div className="MainUI">
+      <div></div>
+      <div className="UsersListArea">
+        <div className="UsersTable" style={{ fontWeight: 'bold' }}>
+          <table>
+            <tr>
+              <th>S. No. </th>
+              <th>Username</th>
+              <th>Name</th>
+              <th>Remove</th>
+            </tr>
+            {rows.map((val, key) => {
+              return (
+                <tr key={key}>
+                  <td>{val.id}</td>
+                  <td>{val.userName}</td>
+                  <td>{val.name}</td>
+                  <td id="RemoveOption">
+                    {' '}
+                    <i
+                      className="fa fa-trash-o"
+                      style={{
+                        color: '#FE7575',
+                        marginLeft: '5px ',
+                        fontSize: '20px',
+                      }}
+                    ></i>
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
