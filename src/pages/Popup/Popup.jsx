a0cb18e87376858e7dacba1f9b4f84ec96e3d6b7
Shrey Gupta <shreygupta@pop-os.localdomain>
2022-04-10 02:36:51 +0530

    popup improved

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,83 +1,27 @@
 import React from 'react';
 import './Popup.css';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    margin: 'auto',
-    maxWidth: 500,
-  },
-  image: {
-    width: 128,
-    height: 128,
-  },
-  img: {
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
-  },
-}));
-
-export function ComplexGrid() {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-          <Grid item>
-            <Avatar>SG</Avatar>
-          </Grid>
-          <Grid item>
-            <Typography gutterBottom variant="subtitle1">
-              React
-            </Typography>
-            <Typography variant="body2" color="textSecondary">
-              @reactjs
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={3} container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs={12} sm={9}>
-                <Grid item>
-                  <Typography gutterBottom variant="subtitle1">
-                    The changes in the React 18 typings go a long way towards
-                    catching issues that were previously silently ignored and
-                    caused crashes.
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Paper>
-    </div>
-  );
-}
+import ComplexGrid from "./Tweet.jsx";
 
 export default function Popup() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>
+          Enlivening
+        </h1>
+        <h3>
+        Don't miss tweets from your favourite content creators
+        </h3>
         <a
           className="App-link"
           href="/options.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Click here to checkout the Options Page!
+          Change your preferences here!
         </a>
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary" style={{margin:"20px"}}>
           Sign in with Twitter!
         </Button>
       </header>
---
+++ src/pages/Popup/Tweet.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: 500,
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+}));
+
+export default function ComplexGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid 
+        container 
+        spacing={2}>
+          <Grid item style={{paddingTop:"15px"}}>
+            <Avatar>RJ</Avatar>
+          </Grid>
+          <Grid item>
+            <Typography gutterBottom p={0} variant="subtitle1">
+              React
+            </Typography>
+            <Typography variant="body2" p={0} color="textSecondary">
+              @reactjs
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={3} container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs={12}>
+                <Grid item>
+                  <Typography variant="subtitle1" align="left">
+                    The changes in the React 18 typings go a long way towards
+                    catching issues that were previously silently ignored and
+                    caused crashes.
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
